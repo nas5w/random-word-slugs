bcaab590643f9898b0ee11031c887cb1fd934ee8
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2021-01-10 21:10:16 -0500

    Begin testing

--- test/randomWordSlugs.test.ts
+++ test/randomWordSlugs.test.ts
@@ -1,3 +1,16 @@
 import randomWordSlugs from "../index";
+import { wordList } from "../words";
 
-describe("randomWordSlugs", () => {});
+const allAdjectives = wordList.adjective.map(({ word }) => word) as string[];
+const allNouns = wordList.noun.map(({ word }) => word) as string[];
+
+describe("randomWordSlugs", () => {
+  it("generates three random kebab case words by default", () => {
+    const slug = randomWordSlugs();
+    const parts = slug.split("-");
+    expect(parts.length).toBe(3);
+    expect(allAdjectives.includes(parts[0])).toBe(true);
+    expect(allAdjectives.includes(parts[1])).toBe(true);
+    expect(allNouns.includes(parts[2])).toBe(true);
+  });
+});
